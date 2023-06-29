20c1411f25b4d68d548017ee5fdc2b047e1047b3
roypromax <roy19rishabh@gmail.com>
2023-06-29 15:08:47 +0530

    cors middleware applied

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const axios = require('axios');
+let cors = require('cors')
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/convert', async (req, res) => {
